821787a991c8faa94164ed3f6bf6a1ea19c73640
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2021-11-07 20:05:44 +0530

    added pr

--- action.js
+++ action.js
@@ -19,7 +19,7 @@ for (let index = 10; index >= 1; index--) {
 let x = 3;
 console.log(x);
 
-/*let c=+prompt();
+let c=+prompt();
 let d=c%10;
 if (d===4) {
     console.log(d + '  having 4 units');
@@ -28,4 +28,40 @@ if (d===4) {
 else{
     console.log(d + '  not having 4 units');
 }
-*/
+
+let a=prompt('enter a number');
+if(a<=96){
+  if (a%2===0) {
+    console.log(a+' is factor of 96');
+    
+  }
+  else{
+    console.log(a+' is not factor of 96');
+  }
+
+}
+else{
+  alert('enter number is grater than 96');
+}
+let i=prompt('enter number');
+if (i%3===0) {
+  console.log(i+ '  is multiple  of 3');
+  
+}
+else{
+  console.log(i+' not multiple of 3');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
