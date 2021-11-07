b7c4184b6ab5c0a7e72dd4296b4f08401273c2dd
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2021-11-07 13:28:56 +0530

    first commit

---
+++ action.js
@@ -0,0 +1,20 @@
+console.log('ravi');
+/*let a= parseInt(prompt());
+if (a%7===0) {
+    console.log(a + "  is devisible by 7");
+    
+}
+else{
+    console.log(a + '  is not divisible by 7');
+}*/
+
+let b=1;
+while(b<=24){
+    
+   console.log(b);
+   b=b+1;
+
+}
+for (let index = 10; index >=1; index--) {
+    console.log(index); 
+}
