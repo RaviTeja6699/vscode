b470a6371a877a3a15427408434b1f45aee0492d
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2021-11-28 13:14:24 +0530

    added loops

---
+++ loops .js
@@ -0,0 +1,91 @@
+/*for(var i=1; i<=5; i++){
+    //document.write(i);
+    //document.write('<br>');
+    //console.log(i);
+
+    for(var j=1; j<=5;j++){
+        //console.log('x');
+       // console.log(j);
+       //document.write(i);
+       //document.write('<br>');
+       //document.write(j)
+
+    }
+   // document.write('<br>');
+}*/
+/*
+//program1
+var k = 1;
+while (k <= 10) {
+  console.log(k);
+  k++;
+}
+//program 2 &4
+var q = 15;
+while (q >= 10) {
+  console.log(q);
+  q--;
+}
+//program 3
+var l = 5;
+while (l <= 15) {
+  console.log(l);
+  l++;
+}
+// program 5
+var t = 1;
+while (t <= 10) {
+  if (t % 2 == 0) {
+    console.log(t);
+  }
+  t++;
+}
+// program 6
+var t = 1;
+while (t <= 10) {
+  if (t % 2 == !0) {
+    console.log(t);
+  }
+  t++;
+}
+// program 7 & 8
+var t = 1;
+while (t <= 10) {
+  var h;
+  h = 4 * t;
+  console.log(h);
+  t++;
+}
+// program 9
+var t = 1;
+while (t <= 24) {
+  var h;
+  h = 24 % t;
+  if (h == 0) {
+    console.log(t);
+  }
+  t++;
+}*/
+//program 10
+var u = 0;
+
+var t = 1;
+while (t <= 24) {
+  if (24 % t === 0) {
+    u=u+1;
+  }
+  t++;
+}
+console.log(u);
+// program 11
+
+
+
+
+
+
+
+
+
+
+
---
+++ objets.js
@@ -0,0 +1,21 @@
+var i;
+i={
+    'name':'ravi',
+    'height':'5.8 inc',
+    'weight':'58kg',
+    
+}
+console.log(typeof(i));
+//console.log(i[1]); // wrong undefind
+console.log(i['name']);
+a=['ravi','34','right hand man','23'];
+console.log(a[0,2]);
+//using varble access the object
+var j='name'
+console.log(i[j]);
+
+for( var g in i)
+{
+    //console.log('hi ', i ,g);
+    console.log('hi',gi[g]);
+}
