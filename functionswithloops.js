3281b5990ad5a12cb0c4fab48444b89b6f6c48c7
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2022-01-09 13:34:07 +0530

    added code

--- functionswithloops.js
+++ functionswithloops.js
@@ -13,3 +13,25 @@ animals.forEach(function(d,f){
     var s=document.getElementById('d1');
     s.appendChild(l)
 })
+//colore change
+function abc(x) {
+    x.target.style.backgroundColor='red'
+}
+// change different colors with one button
+function makeit(c){
+    var q= document.getElementById('d3');
+    q.style.backgroundColor=c;
+}
+var u=['ravi','3','teja','geetha']
+document.getElementById('d4').innerHTML=u[0];
+i=1;
+function nextitem(){
+    var w= document.getElementById('d4');
+    w.innerHTML=u[i];
+    if (i<3) {
+        i++;
+    } else {
+        i=0;
+    }
+
+}
