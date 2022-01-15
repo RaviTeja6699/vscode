46a73e34e4722a1a858e7271549bcaf44eca1a7b
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2022-01-15 21:23:42 +0530

    added table sorting

--- examples/sortin.js
+++ examples/sortin.js
@@ -15,16 +15,55 @@ var students =[
         Place:'ka'
     }
     ]
-    students.forEach(function(a,b){
-        var ntr = document.createElement("tr");// document "d " should be small d only
-        var std = document.createElement("td");
-        std.innerHTML=b+1;
-        ntr.appendChild(std);
-        for(k in a){
-            var ntd=document.createElement("td");
-            ntd.innerHTML=a[k];
-            ntr.appendChild(ntd);
+    genUI();//for first time loading
+    function genUI() {
+        students.forEach(function(a,b){
+            var ntr = document.createElement("tr");// document "d " should be small d only
+            var std = document.createElement("td");
+            std.innerHTML=b+1;
+            ntr.appendChild(std);
+            for(k in a){
+                var ntd=document.createElement("td");
+                ntd.innerHTML=a[k];
+                ntr.appendChild(ntd);
+            }
+            document.getElementById("d4").appendChild(ntr);
+            console.log(students);
+        })
+        
+    }
+    function sortby(k){
+        document.getElementById('d4').innerHTML="";
+        students.sort(function(c,d){
+        if (c[k]>d[k]) {
+            return 1
         }
-        document.getElementById("d4").appendChild(ntr);
-        console.log(students);
+        else{
+            return -1
+        }
+        })
+        genUI();
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    // sort functon is do only string sort so  for nember sorting we use below code
+    var nr=[23,4,5,6,7,8,23,45,67,89]
+    nr.sort(function(a,b) {
+        if (a>b) {
+            return 1
+        } else {
+            return -1
+        }
     })
+    console.log(nr)
