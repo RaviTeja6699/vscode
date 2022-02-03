cbf1a572d0c71d63472d877eb6ed867a6c348be9
Ravi Teja <84436776+RaviTeja6699@users.noreply.github.com>
2022-02-03 06:35:00 +0530

    destracture

--- refference.js
+++ refference.js
@@ -1,10 +1,20 @@
 var ar=[1,2,3,4];
-var br = ar;
+// var br = ar;
 
-br[1]='ravi';
-console.log(br);
-console.log(ar);
-var cr=[...br];//this is the way to creat new array this is called cloaning
-br[0]='teja';
-console.log(br);
+// br[1]='ravi';
+// console.log(br);
+// console.log(ar);
+// var cr=[...br];//this is the way to creat new array this is called cloaning
+// br[0]='teja';
+// console.log(br);
+// console.log(cr);
+// var er=[];
+// er=ar.map(function(n,i){
+//    return n; 
+// })
+// console.log(er);
+// destracring
+var ar1=['ravi','teja'];
+var cr=[...ar,...ar1]
 console.log(cr);
+
